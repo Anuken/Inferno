a60630dfbb7b8d3fd71632b66a8eb1b5e963980b
Anuken <arnukren@gmail.com>
2019-06-28 15:34:48 -0400

    yÿ

--- core/src/inferno/type/Boss.java
+++ core/src/inferno/type/Boss.java
@@ -1,10 +1,11 @@
 package inferno.type;
 
 import inferno.entity.SolidEntity;
+import inferno.graphics.Layer;
 import io.anuke.arc.Core;
 import io.anuke.arc.graphics.Color;
 import io.anuke.arc.graphics.g2d.*;
-import io.anuke.arc.math.Angles;
+import io.anuke.arc.math.*;
 import io.anuke.arc.math.geom.Rectangle;
 
 public class Boss extends Char{
@@ -16,9 +17,16 @@ public class Boss extends Char{
     }
 
     @Override
+    public void update(){
+
+    }
+
+    @Override
     public void draw(){
         TextureRegion region = Core.atlas.find("lucine");
         Draw.rect(region, x, y + region.getHeight()/2f);
+
+        Layer.light(x, y + height(), 160f, Color.SCARLET);
     }
 
     @Override
