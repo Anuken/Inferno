d4aca5fdc3f0ce7d446974258e826470e06a9758
Anuken <arnukren@gmail.com>
2019-07-07 09:48:10 -0400

    unspeed

--- core/src/inferno/Control.java
+++ core/src/inferno/Control.java
@@ -4,24 +4,18 @@ import inferno.type.Boss;
 import inferno.type.Player;
 import io.anuke.arc.ApplicationListener;
 import io.anuke.arc.Core;
-import io.anuke.arc.audio.Music;
 import io.anuke.arc.input.KeyCode;
-import io.anuke.arc.math.Interpolation;
-import io.anuke.arc.math.Mathf;
 import io.anuke.arc.util.Time;
 
 import static inferno.Inferno.*;
 
 public class Control implements ApplicationListener{
     private boolean paused = false;
-    private Music music = Core.audio.newMusic(Core.files.internal("music/out.ogg"));
 
     public Control(){
-        Time.setDeltaProvider(() -> Mathf.clamp(music.getPosition() < 34f ? Interpolation.pow2In.apply(music.getPosition() / 34f) * 2f : 2f + (music.getPosition()-34f) / 25f, 0f, 5f));
         Core.keybinds.setDefaults(Binding.values());
         Core.settings.setAppName("Inferno");
         Core.settings.load();
-        music.play();
     }
 
     @Override
