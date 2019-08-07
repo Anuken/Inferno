b983e05bc73ad39f514b233cbe0a7cef7879d97a
Anuken <arnukren@gmail.com>
2019-08-07 10:20:15 -0400

    TeaVM backend, first commit

--- core/src/inferno/type/Phases.java
+++ core/src/inferno/type/Phases.java
@@ -562,8 +562,8 @@ public class Phases{
                 int current = (special++) % cycle.size;
                 if(current == 0){
                     float aim =  boss.aim();
-                    loop(20, i -> {
-                        run(i * 2, () -> shotgun(3, 15f, aim + sin(i, 1f, 3f), boss::shoot));
+                    loop(40, i -> {
+                        run(i * 2, () -> shotgun(3, 15f, aim + sin(i, 3f, 5f), boss::shoot));
                     });
                 }else{
                     cycle.get(current).run();
