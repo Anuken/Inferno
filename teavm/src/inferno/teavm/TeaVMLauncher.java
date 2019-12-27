c8c4ce07f239ef62ae478b25316ca227581a986e
Anuken <arnukren@gmail.com>
2019-12-27 14:00:51 -0500

    2

--- teavm/src/inferno/teavm/TeaVMLauncher.java
+++ teavm/src/inferno/teavm/TeaVMLauncher.java
@@ -1,16 +1,10 @@
 package inferno.teavm;
 
+import arc.backend.teavm.*;
+import arc.backend.teavm.TeaApplication.*;
 import inferno.*;
-import io.anuke.arc.*;
-import io.anuke.arc.backends.teavm.*;
-import io.anuke.arc.backends.teavm.TeaApplication.*;
-import io.anuke.arc.graphics.*;
-import io.anuke.arc.graphics.VertexAttributes.*;
-import io.anuke.arc.graphics.g2d.*;
-import io.anuke.arc.graphics.glutils.*;
 import org.teavm.jso.browser.*;
 import org.teavm.jso.dom.html.*;
-import org.teavm.jso.dom.xml.*;
 
 public class TeaVMLauncher {
     public static void main(String[] args) {
